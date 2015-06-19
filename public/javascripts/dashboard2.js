aa5ca226cfe7d968535e707c43d0a5bde62e1251
Harmeet Singh <harmeet4855@gmail.com>
2015-06-19 18:00:38 +0530

    frontend layout with styling

---
+++ public/javascripts/dashboard2.js
@@ -0,0 +1,140 @@
+'use strict';
+$(function () {
+
+  /* ChartJS
+   * -------
+   * Here we will create a few charts using ChartJS
+   */
+  //
+  //-------------
+  //- BAR CHART -
+  //-------------
+  var areaChartData = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "Electronics",
+        fillColor: "rgba(210, 214, 222, 1)",
+        strokeColor: "rgba(210, 214, 222, 1)",
+        pointColor: "rgba(210, 214, 222, 1)",
+        pointStrokeColor: "#c1c7d1",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [65, 59, 80, 81, 56, 55, 40]
+      },
+      {
+        label: "Digital Goods",
+        fillColor: "rgba(60,141,188,0.9)",
+        strokeColor: "rgba(60,141,188,0.8)",
+        pointColor: "#3b8bba",
+        pointStrokeColor: "rgba(60,141,188,1)",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(60,141,188,1)",
+        data: [28, 48, 40, 19, 86, 27, 90]
+      }
+    ]
+  };
+
+
+  var barChartCanvas = $("#barChart").get(0).getContext("2d");
+  var barChart = new Chart(barChartCanvas);
+  var barChartData = areaChartData;
+  barChartData.datasets[1].fillColor = "#00a65a";
+  barChartData.datasets[1].strokeColor = "#00a65a";
+  barChartData.datasets[1].pointColor = "#00a65a";
+  var barChartOptions = {
+    //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+    scaleBeginAtZero: true,
+    //Boolean - Whether grid lines are shown across the chart
+    scaleShowGridLines: true,
+    //String - Colour of the grid lines
+    scaleGridLineColor: "rgba(0,0,0,.05)",
+    //Number - Width of the grid lines
+    scaleGridLineWidth: 1,
+    //Boolean - Whether to show horizontal lines (except X axis)
+    scaleShowHorizontalLines: true,
+    //Boolean - Whether to show vertical lines (except Y axis)
+    scaleShowVerticalLines: true,
+    //Boolean - If there is a stroke on each bar
+    barShowStroke: true,
+    //Number - Pixel width of the bar stroke
+    barStrokeWidth: 2,
+    //Number - Spacing between each of the X value sets
+    barValueSpacing: 5,
+    //Number - Spacing between data sets within X values
+    barDatasetSpacing: 1,
+    //String - A legend template
+    legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].fillColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
+    //Boolean - whether to make the chart responsive
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  barChartOptions.datasetFill = false;
+  barChart.Bar(barChartData, barChartOptions);
+
+   /* jVector Maps
+   * ------------
+   * Create a world map with markers
+   */
+  $('#world-map-markers').vectorMap({
+    map: 'world_mill_en',
+    normalizeFunction: 'polynomial',
+    hoverOpacity: 0.7,
+    hoverColor: false,
+    backgroundColor: 'transparent',
+    regionStyle: {
+      initial: {
+        fill: 'rgba(210, 214, 222, 1)',
+        "fill-opacity": 1,
+        stroke: 'none',
+        "stroke-width": 0,
+        "stroke-opacity": 1
+      },
+      hover: {
+        "fill-opacity": 0.7,
+        cursor: 'pointer'
+      },
+      selected: {
+        fill: 'yellow'
+      },
+      selectedHover: {
+      }
+    },
+    markerStyle: {
+      initial: {
+        fill: '#00a65a',
+        stroke: '#111'
+      }
+    },
+    markers: [
+      {latLng: [41.90, 12.45], name: 'Vatican City'},
+      {latLng: [43.73, 7.41], name: 'Monaco'},
+      {latLng: [-0.52, 166.93], name: 'Nauru'},
+      {latLng: [-8.51, 179.21], name: 'Tuvalu'},
+      {latLng: [43.93, 12.46], name: 'San Marino'},
+      {latLng: [47.14, 9.52], name: 'Liechtenstein'},
+      {latLng: [7.11, 171.06], name: 'Marshall Islands'},
+      {latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis'},
+      {latLng: [3.2, 73.22], name: 'Maldives'},
+      {latLng: [35.88, 14.5], name: 'Malta'},
+      {latLng: [12.05, -61.75], name: 'Grenada'},
+      {latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines'},
+      {latLng: [13.16, -59.55], name: 'Barbados'},
+      {latLng: [17.11, -61.85], name: 'Antigua and Barbuda'},
+      {latLng: [-4.61, 55.45], name: 'Seychelles'},
+      {latLng: [7.35, 134.46], name: 'Palau'},
+      {latLng: [42.5, 1.51], name: 'Andorra'},
+      {latLng: [14.01, -60.98], name: 'Saint Lucia'},
+      {latLng: [6.91, 158.18], name: 'Federated States of Micronesia'},
+      {latLng: [1.3, 103.8], name: 'Singapore'},
+      {latLng: [1.46, 173.03], name: 'Kiribati'},
+      {latLng: [-21.13, -175.2], name: 'Tonga'},
+      {latLng: [15.3, -61.38], name: 'Dominica'},
+      {latLng: [-20.2, 57.5], name: 'Mauritius'},
+      {latLng: [26.02, 50.55], name: 'Bahrain'},
+      {latLng: [0.33, 6.73], name: 'São Tomé and Príncipe'}
+    ]
+  });
+
+});
---
+++ public/javascripts/main.js
@@ -0,0 +1,143 @@
+/* AdminLTE
+ *
+ * @type Object
+ * @description $.AdminLTE is the main object for the template's app.
+ *              It's used for implementing functions and options related
+ *              to the template. Keeping everything wrapped in an object
+ *              prevents conflict with other plugins and is a better
+ *              way to organize our code.
+ */
+$.AdminLTE = {};
+
+$.AdminLTE.options = {
+    //Box Widget Plugin. Enable this plugin
+    //to allow boxes to be collapsed and/or removed
+    enableBoxWidget: true,
+    //Box Widget plugin options
+    boxWidgetOptions: {
+        boxWidgetIcons: {
+            //Collapse icon
+            collapse: 'fa-minus',
+            //Open icon
+            open: 'fa-plus',
+            //Remove icon
+            remove: 'fa-times'
+        },
+        boxWidgetSelectors: {
+            //Remove button selector
+            remove: '[data-widget="remove"]',
+            //Collapse button selector
+            collapse: '[data-widget="collapse"]'
+        }
+    },
+    //Define the set of colors to use globally around the website
+    colors: {
+        lightBlue: "#3c8dbc",
+        red: "#f56954",
+        green: "#00a65a",
+        aqua: "#00c0ef",
+        yellow: "#f39c12",
+        blue: "#0073b7",
+        navy: "#001F3F",
+        teal: "#39CCCC",
+        olive: "#3D9970",
+        lime: "#01FF70",
+        orange: "#FF851B",
+        fuchsia: "#F012BE",
+        purple: "#8E24AA",
+        maroon: "#D81B60",
+        black: "#222222",
+        gray: "#d2d6de"
+    },
+    //The standard screen sizes that bootstrap uses.
+    //If you change these in the variables.less file, change
+    //them here too.
+    screenSizes: {
+        xs: 480,
+        sm: 768,
+        md: 992,
+        lg: 1200
+    }
+};
+
+
+/* ------------------
+ * - Implementation -
+ * ------------------
+ * The next block of code implements AdminLTE's
+ * functions and plugins as specified by the
+ * options above.
+ */
+$(function (){
+
+//Easy access to options
+    var o = $.AdminLTE.options;
+    _init();
+    //Activate box widget
+    if (o.enableBoxWidget) {
+        $.AdminLTE.boxWidget.activate();
+    }
+
+});
+
+function _init() {
+    /* BoxWidget
+     * =========
+     * BoxWidget is a plugin to handle collapsing and
+     * removing boxes from the screen.
+     *
+     * @type Object
+     * @usage $.AdminLTE.boxWidget.activate()
+     *        Set all your options in the main $.AdminLTE.options object
+     */
+    $.AdminLTE.boxWidget = {
+        selectors: $.AdminLTE.options.boxWidgetOptions.boxWidgetSelectors,
+        icons: $.AdminLTE.options.boxWidgetOptions.boxWidgetIcons,
+        activate: function () {
+            var _this = this;
+            //Listen for collapse event triggers
+            $(_this.selectors.collapse).on('click', function (e) {
+                e.preventDefault();
+                _this.collapse($(this));
+            });
+
+            //Listen for remove event triggers
+            $(_this.selectors.remove).on('click', function (e) {
+                e.preventDefault();
+                _this.remove($(this));
+            });
+        },
+        collapse: function (element) {
+            var _this = this;
+            //Find the box parent
+            var box = element.parents(".box").first();
+            //Find the body and the footer
+            var box_content = box.find("> .box-body, > .box-footer");
+            if (!box.hasClass("collapsed-box")) {
+                //Convert minus into plus
+                element.children(":first")
+                    .removeClass(_this.icons.collapse)
+                    .addClass(_this.icons.open);
+                //Hide the content
+                box_content.slideUp(300, function () {
+                    box.addClass("collapsed-box");
+                });
+            } else {
+                //Convert plus into minus
+                element.children(":first")
+                    .removeClass(_this.icons.open)
+                    .addClass(_this.icons.collapse);
+                //Show the content
+                box_content.slideDown(300, function () {
+                    box.removeClass("collapsed-box");
+                });
+            }
+        },
+        remove: function (element) {
+            //Find the box parent
+            var box = element.parents(".box").first();
+            box.slideUp();
+        }
+    };
+}
+
