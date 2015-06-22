3b5b1549661c38b9e48186633cef08465e2fe4c0
Rahul Kumar <rahulkumar.aws@gmail.com>
2015-06-22 17:11:06 +0530

    main Js function added

---
+++ app/Global.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by rahul on 18/06/15.
+ */
+object Global {
+
+}
---
+++ public/javascripts/app/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rahul on 22/06/15.
+ */
+
+var project3dApp = angular.module('demoApp',['ui.bootstrap','ngTable'])
+
+project3dApp.controller('demoAppCtrl',function($scope,$http){
+
+
+})
